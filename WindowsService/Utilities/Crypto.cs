6a409b72cd62b341ae413274efde4fdc4e7848eb
vladimir Novick <vlad.novick@gmail.com>
2018-07-09 22:48:06 +0300

    Porting to .NET Framework 4.6.1

--- WindowsService/Utilities/Crypto.cs
+++ WindowsService/Utilities/Crypto.cs
@@ -1,13 +1,29 @@
-﻿////////////////////////////////////////////////////////////////////////////
-//	Copyright 2014 : Vladimir Novick    https://www.linkedin.com/in/vladimirnovick/  
-//
-//    NO WARRANTIES ARE EXTENDED. USE AT YOUR OWN RISK. 
-//
-//      Available under the BSD and MIT licenses
-//
-// To contact the author with suggestions or comments, use  :vlad.novick@gmail.com
-//
-////////////////////////////////////////////////////////////////////////////
+﻿/*
+
+Copyright (C) 2014-2018 by Vladimir Novick http://www.linkedin.com/in/vladimirnovick ,
+
+    vlad.novick@gmail.com , http://www.sgcombo.com , https://github.com/Vladimir-Novick
+	
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+*/
 using System;
 using System.Text;
 using System.IO;
